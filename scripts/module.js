c0e41eafe2e5b7d30b760b4ab548e61f4212b86f
7H3LaughingMan <7H3LaughingMan@proton.me>
2024-06-14 18:12:09 +0000

    Add Additional Color Settings

--- scripts/module.js
+++ scripts/module.js
@@ -30,13 +30,48 @@ const Unreachable = {
 
 Hooks.once("ready", () => {
     new window.Ardittristan.ColorSetting("pf2e-elevation-ruler", "singleAction", {
-        name: "My Color Setting",           // The name of the setting in the settings menu
-        hint: "Click on the button",        // A description of the registered setting and its behavior
+        name: "Single Action Color",           // The name of the setting in the settings menu
         label: "Color Picker",              // The text label used in the button
         restricted: false,                  // Restrict this setting to gamemaster only?
-        defaultColor: "#000000ff",          // The default color of the setting
+        defaultColor: "#3222C7",          // The default color of the setting
         scope: "client",                    // The scope of the setting
         onChange: (value) => {}            // A callback function which triggers when the setting is changed
+    })
+
+    new window.Ardittristan.ColorSetting("pf2e-elevation-ruler", "doubleAction", {
+        name: "Double Action Color",           // The name of the setting in the settings menu
+        label: "Color Picker",              // The text label used in the button
+        restricted: false,                  // Restrict this setting to gamemaster only?
+        defaultColor: "#FFEC07",          // The default color of the setting
+        scope: "client",                    // The scope of the setting
+        onChange: (value) => {}            // A callback function which triggers when the setting is changed
+    })
+
+    new window.Ardittristan.ColorSetting("pf2e-elevation-ruler", "tripleAction", {
+        name: "Triple Action Color",           // The name of the setting in the settings menu
+        label: "Color Picker",              // The text label used in the button
+        restricted: false,                  // Restrict this setting to gamemaster only?
+        defaultColor: "#C033E0",          // The default color of the setting
+        scope: "client",                    // The scope of the setting
+        onChange: (value) => {}            // A callback function which triggers when the setting is changed
+    })
+
+    new window.Ardittristan.ColorSetting("pf2e-elevation-ruler", "quadrupleAction", {
+        name: "Quadruple Action Color",           // The name of the setting in the settings menu
+        label: "Color Picker",              // The text label used in the button
+        restricted: false,                  // Restrict this setting to gamemaster only?
+        defaultColor: "#1BCAD8",          // The default color of the setting
+        scope: "client",                    // The scope of the setting
+        onChange: (value) => {}            // A callback function which triggers when the setting is changed
+    })
+
+    new window.Ardittristan.ColorSetting("pf2e-elevation-ruler", "unreachable", {
+        name: "Unreachable",
+        label: "Color Picker",
+        restricted: false,
+        defaultColor: "#FF0000",
+        scope: "client",
+        onChange: (value) => {}
     })
 
     CONFIG.elevationruler.SPEED.CATEGORIES = [ SingleAction, DoubleAction, TripleAction, QuadrupleAction, Unreachable ];
